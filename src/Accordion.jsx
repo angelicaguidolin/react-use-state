1146dbe6dcece9398519bc54f8720f9f7c2efd9b
angelica <guidolinangelica66@gmail.com>
2025-02-09 21:47:41 +0100

    feat add accordion list

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,13 +1,20 @@
-import accpordion from "../data/accpordion"
+import accordion from "../data/accordion"
 
 export default function Accordion(){
     return(
-        <div className="box-acc">
-            <div className="title">
-                <h3>TITLE</h3>
-                <button className="accordion-btn"></button>
-            </div>
-            <div>contenuto</div>
+        <div className="list-acc">
+            {accordion.map((accord, id)=>{
+                return(
+                    <div key={id} className="box-acc">
+                    <div className="title">
+                        <h3>{accord.title}</h3>
+                        <button className="accordion-btn">+</button>
+                    </div>
+                    <div>{accord.description}</div>
+                </div>
+                )
+            })}
+       
         </div>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
-export default function App(){}
+import Accordion from "./Accordion";
+
+export default function App(){
+    return(
+        <>
+        <div className="container">
+        <Accordion/>
+        </div>
+        </>
+    )
+}
